5cd077dfd953766be7091c8c4f330a0ee22f3c88
DanniMtz <odamar_va@hotmail.com>
2024-07-15 22:16:52 -0600

    Variables y Comentarios

--- Curso01/app.js
+++ Curso01/app.js
@@ -1,8 +1,16 @@
+// Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor");
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor");
 
 console.log(numeroUsuario);
 
+/*Este código
+ realiza la comparación
+ */
 if (numeroUsuario == numeroSecreto){
+    // acertamos, condición Verdadera
     alert('Acertaste el número');
+} else {
+    // No acertaste, condición Falsa
+    alert('Lo siento, no acerptaste el número secreto')
 }
