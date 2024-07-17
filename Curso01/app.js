7246bf35ddef0dafd142a41e128924b62f7d165d
DanniMtz <odamar_va@hotmail.com>
2024-07-16 22:44:41 -0600

    Arreglo palabra veces

--- Curso01/app.js
+++ Curso01/app.js
@@ -2,6 +2,7 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
+let palabraVeces = 'vez'
 
 while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente continua
     numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor");
@@ -10,7 +11,7 @@ while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente
 
     if (numeroUsuario == numeroSecreto){ // Este código  realiza la comparación 
         // acertamos, condición Verdadera
-        alert(`Acertaste,  el número es: ${numeroUsuario}. Lo hiciste en ${intentos} veces`); // Template Strings
+        alert(`Acertaste,  el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`); // Template Strings
     } else {
         // se compara el numero usuario e indica si es menor que el numero secreto
         if(numeroUsuario > numeroSecreto){
@@ -21,6 +22,7 @@ while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente
         // No acertaste, condición Falsa
         // alert('Lo siento, no acerptaste el número secreto');
         intentos = intentos + 1;
+        palabraVeces ='veces';
     }
 }
 
