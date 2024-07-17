58d864aa31a999030303f813d135c88602bb5a0f
DanniMtz <odamar_va@hotmail.com>
2024-07-17 00:05:41 -0600

    Números máximo y minimo ingresado por el usuario

--- Curso01/app.js
+++ Curso01/app.js
@@ -1,14 +1,27 @@
+//Funcion para generar un numero de entero aleatorio entre un dado
+function generarNumeroAleatorio(min,max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
 // Variables
-let numeroSecreto = Math.floor(Math.random()*10)+1; // .floor obtiene el entero, + 1 para no incluir el 0 (1-10)
+let numeroSecreto = 0 // Math.floor(Math.random()*10)+1; // .floor obtiene el entero, + 1 para no incluir el 0 (1-10)
 let numeroUsuario = 0;
 let intentos = 1;
 //let palabraVeces = 'vez';
 let maximoIntentos = 3
 
-while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente continua
-    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10 por favor"));
+// solicitar max y min
+let numeroMax = parseInt(prompt("Ingresa un numero Maximo:"));
+let numeroMin = parseInt(prompt("Ingresa un numero Minimo:"));
 
-    console.log(numeroUsuario);
+numeroSecreto = generarNumeroAleatorio(numeroMin,numeroMax);
+
+while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente continua
+    numeroUsuario = // parseInt(prompt("Me indicas un número entre 1 y 10 por favor")); // version anterior
+                    parseInt(prompt(`Me indicas un número entre ${numeroMin} y ${numeroMax}  por favor:`));
+
+    console.log(typeof(numeroUsuario)); //  obtener el tipo de dato
 
     if (numeroUsuario == numeroSecreto){ // Este código  realiza la comparación 
         // acertamos, condición Verdadera
