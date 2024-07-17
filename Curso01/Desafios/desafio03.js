30b9979a77673e909f63a40d75f0380efa9f919e
DanniMtz <odamar_va@hotmail.com>
2024-07-16 23:18:46 -0600

    Desafio 03 Completado

---
+++ Curso01/Desafios/desafio03.js
@@ -0,0 +1,26 @@
+
+// Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+/*
+let contador = 1;
+while (contador <= 10){
+    alert (`El contador esta en ${contador}`);
+    contador = contador + 1;
+}
+*/
+
+// Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
+/*
+let contador2 = 10;
+while (contador2 >= 0){
+    alert (`El contador esta en ${contador2}`);
+    contador2 = contador2 - 1;
+}
+*/
+
+// Crea un programa de cuenta progresiva. Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+let numeroUsuario = prompt('Ingresa un número para realizar cuenta regresiva')
+while (numeroUsuario >=0){
+    alert(`El numero se encuentra en: ${numeroUsuario}`);
+    numeroUsuario = numeroUsuario - 1
+
+}
