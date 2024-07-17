3fdf8c36d3a996b3f4de89b0e6a17cf2fd30f214
DanniMtz <odamar_va@hotmail.com>
2024-07-16 22:37:59 -0600

    Actualizacion Contador Inentos

--- Curso01/app.js
+++ Curso01/app.js
@@ -1,6 +1,7 @@
 // Variables
 let numeroSecreto = 6;
 let numeroUsuario = 0;
+let intentos = 1;
 
 while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente continua
     numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor");
@@ -9,7 +10,7 @@ while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente
 
     if (numeroUsuario == numeroSecreto){ // Este código  realiza la comparación 
         // acertamos, condición Verdadera
-        alert(`Acertaste,  el número es: ${numeroUsuario}`); // Template Strings
+        alert(`Acertaste,  el número es: ${numeroUsuario}. Lo hiciste en ${intentos} veces`); // Template Strings
     } else {
         // se compara el numero usuario e indica si es menor que el numero secreto
         if(numeroUsuario > numeroSecreto){
@@ -19,6 +20,7 @@ while (numeroUsuario != numeroSecreto) { // Mientras numeroUsuario sea Diferente
         }
         // No acertaste, condición Falsa
         // alert('Lo siento, no acerptaste el número secreto');
+        intentos = intentos + 1;
     }
 }
 
