12468102e5da5eb0661e950022ce91a0f624aaeb
DanniMtz <odamar_va@hotmail.com>
2024-07-18 23:23:33 -0600

    Conexión HTML por Selectores

--- Curso02/app.js
+++ Curso02/app.js
@@ -1,5 +1,8 @@
+// conexion a HTML por Selectores de Objetos
 let titulo = document.querySelector('h1');
+// asigna un titulo a la etiqueta h1 con innerHTML
 titulo.innerHTML = 'Juego del número secreto';
 
+// Asinamos a etiqueta parrafo un Texto
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un número del 1 al 10';
